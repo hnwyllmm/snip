9634e528b41abb9139f05be51bb8196d2a549656
Laughing <hnwyllmm@126.com>
2017-11-03 17:37:59 +0800

    readn/writen
    封装一次读取/写入N字节

---
+++ read_write_n.h
@@ -0,0 +1,42 @@
+int writen(int fd, const void *buf, int size)
+{
+	const char *tmp = (const char *)buf;
+	while ( size > 0)
+	{
+		const ssize_t ret = ::write(fd, tmp, size);
+		if (ret >= 0)
+		{
+			tmp  += ret;
+			size -= ret;
+			continue;
+		}
+
+		const int err = errno;
+		if (EAGAIN != err && EINTR != err)
+			return err;
+	}
+	return 0;
+}
+
+int readn(int fd, void *buf, int size)
+{
+	char *tmp = (char *)buf;
+	while (size > 0)
+	{
+		const ssize_t ret = ::read(fd, tmp, size);
+		if (ret >= 0)
+		{
+			tmp  += ret;
+			size -= ret;
+			continue;
+		}
+
+		if (0 == ret)
+			return -1; // end of file
+
+		const int err = errno;
+		if (EAGAIN != err && EINTR != err)
+			return err;
+	}
+	return 0;
+}
