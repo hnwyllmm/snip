e14d388ff66f10bdad233ea900df6f1bf572f446
Laughing <hnwyllmm@126.com>
2019-09-12 10:20:15 +0800

    增加文件选项
    增加-f文件选项，支持将存放十六进制数据的文件转换为二进制文件。
    十六进制文件只关心十六进制字符，包括'0' - '9', 'a' - 'f'和'A' - 'F'。不关心其它字符，比如空格、换行等。所以十六进制数据文件中无需调整格式。
    比如：
    72AC 23D1
    同时也可以
    72AC23D1
    或者两种格式在同一个文件

--- hex2bin.py
+++ hex2bin.py
@@ -48,7 +48,7 @@ def hexstr2bin(hexdata, output_file):
 		return
 
 	output_file.write(result)
-	print("valid character number is " + str(count) + ", result size " + str(len(result)))
+	#print("valid character number is " + str(count) + ", result size " + str(len(result)))
 
 
 if __name__ == "__main__":
@@ -57,13 +57,21 @@ if __name__ == "__main__":
 		options_parser = OptionParser()
 		options_parser.add_option('-x', '--hex', action='store', type='string',
 			dest='hexdata', default=None, help='data string in hex format')
+		options_parser.add_option('-f', '--file', action='store', type='string',
+			dest='file', default=None, help='file with hex data')
 		options_parser.add_option('-o', '--output', action='store', type='string',
 			dest='output', default='output.txt', help='output file')
 
 		(options, args) = options_parser.parse_args(sys.argv[1:])
 
 		with open(options.output, 'wb') as output_file:
-			hexstr2bin(options.hexdata, output_file)
+			if options.file != None:
+				with open(options.file, 'rb') as source_file:
+					for line in source_file.readlines():
+						hexstr2bin(line, output_file)
+			else:
+				hexstr2bin(options.hexdata, output_file)
+
 
 	except Exception as ex:
 		print("exception: " + str(ex))
