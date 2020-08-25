ded374921ec6e78d80d348c1c72f4ca9103e0732
wangyunlai <wangyunlai@youzan.com>
2020-08-25 09:37:03 +0800

    stack_sum 支持java 1.8 hotspot jstack结果

--- stack_sum.py
+++ stack_sum.py
@@ -7,6 +7,7 @@ import sys
 try:
 	from optparse import OptionParser
 except:
+	print("cannot load optparse module")
 	exit(1)
 
 user_input = None
@@ -70,6 +71,9 @@ class thread_info_t:
 		self.tid = tid
 		self.pthreadId = pthreadId
 		self.desc = []
+
+	def add_desc(self, desc):
+		self.desc.append(desc)
 
 	def __str__(self):
 		res = "tid:" + str(self.tid).rjust(16) + ", pthreadId:" + str(self.pthreadId).rjust(8)
@@ -164,6 +168,8 @@ class thread_info_intf_t:
 	def thread_info(self, line):
 		pass
 
+	# si 是一个数组，第一个元素是thread_info信息，后面的元素是线程栈的每个frame
+	# signature相同的栈，就会汇总在一起
 	def signature(self, si):
 		'''能唯一标识一个栈'''
 		pass
@@ -379,13 +385,109 @@ class thread_info_hpux_t(thread_info_intf_t):
 
 		return func
 
+class thread_info_java_t(thread_info_intf_t):
+	def __init__(self):
+		thread_info_intf_t.__init__(self)
+
+	# Thread 26725: (state = BLOCKED)
+	def is_new_thread(self, line):
+		if line.find('Thread ') != -1 and line.find('(state = '):
+			return True
+
+		return False
+
+	def thread_info(self, line):
+		arr = line.split()
+		if len(arr) >= 5:
+			threadid = arr[1][0:-2]
+			state = 'state = ' + arr[4][0:-2]
+			thinfo = thread_info_t(arr[1], arr[1])
+			thinfo.add_desc(state)
+			return thinfo
+
+		thinfo = thread_info_t(-1, -1)
+		return thinfo
+
+	# 每个frame上的函数串在一起作为signature
+	def signature(self, si):
+		ret = ''
+		for line in si.lines()[1:]:
+			line = line.strip()
+			if len(line) == 0:
+				continue
+			func = self.get_function(line)
+			ret  += func + ';'
+		return ret
+
+	def get_function(self, line):
+		start = line.find('(')
+		if -1 == start:
+			return ''
+
+		#  - java.util.concurrent.ThreadPoolExecutor$Worker.run() @bci=5, line=624 (Compiled frame)
+		func = line[start + 3:] # start with ' - '
+		index = func.find('(')
+		if -1 != index:
+			func = func[0:index]
+		
+		return func
+
+class thread_info_java_hotspot_t(thread_info_intf_t):
+	def __init__(self):
+		thread_info_intf_t.__init__(self)
+
+	# "pool-1-thread-58114" #294535 prio=5 os_prio=0 tid=0x00007fb1886f7800
+	# nid=0x72c8 waiting on condition [0x00007fae0e1ec000]
+	def is_new_thread(self, line):
+		if line.find('prio=') != -1 and line.find('tid=') and line.find('nid='):
+			return True
+
+		return False
+
+	def thread_info(self, line):
+		arr = line.split()
+		if len(arr) >= 2:
+			thread_name = arr[0][1:-2]
+			thread_id = arr[1][1:]
+			thinfo = thread_info_t(thread_id, thread_id) 
+			thinfo.add_desc('thread_name = ' + thread_name + ' thread id = ' + thread_id)
+			return thinfo
+
+		thinfo = thread_info_t(-1, -1)
+		return thinfo
+
+	# 每个frame上的函数串在一起作为signature
+	def signature(self, si):
+		ret = ''
+		if len(si.lines()) <= 2:
+			return ret
+
+		for line in si.lines()[2:]:
+			line = line.strip()
+			if len(line) == 0:
+				continue
+			func = self.get_function(line)
+			ret  += func + ';'
+		return ret
+
+	def get_function(self, line):
+	    	# at
+		# java.util.concurrent.locks.LockSupport.parkNanos(LockSupport.java:215)
+	    	if line.startswith('at ') == False:
+			return ''
+
+		return line
+
+###############################################################################
+# 根据栈信息，猜测是哪个平台
 class stack_anlyse_intf_t:
 
 	def __init__(self):
 		self.__os_type = ''
 
+
 	def probe(self, file):
-		'''判断操作系统类型'''
+		'''判断平台环境，并返回对应的线程分析对象'''
 		if file == None:
 			return None
 
@@ -410,16 +512,40 @@ class stack_anlyse_intf_t:
 				ret = thread_info_gdb_t()
 				print('gdb')
 				return ret
+
+			# JVM version is 25.201-b09
+			if line.find('JVM version is') != -1:
+				self.__os_type = 'Java'
+				ret = thread_info_java_t()
+				print("Java virtual machine")
+				return ret
+
+			# Full thread dump Java HotSpot(TM) 64-Bit Server VM
+			# (25.151-b12 mixed mode):
+			if line.find('Java HotSpot') != -1:
+				self.__os_type = 'Java HotSpot'
+				ret = thread_info_java_hotspot_t()
+    				print("Java HotSpot")
+				return ret
+
 		print('unsupported stack format')
 		return None
 
 class StackParser:
 
 	def __init__(self, file="pstack.txt"):
+		# 栈信息源文件
 		self.__file               = file
+
+		# 所有的线程栈信息
 		self.__stack_list         = []
-		self.__thread_info_parser = thread_info_intf_t()
+
+		# 拥有相同栈信息的线程汇总到一起，
+		# 第一个元素记录了栈信息
+		# 第二个元素记录了这个线程栈出现的次数
 		self.__stack_summary      = {}
+
+		# 线程分析接口
 		self.__thread_info_intf   = None
 
 	def __str__(self):
@@ -427,18 +553,20 @@ class StackParser:
 
 	def __preparse(self):
 		self.__stack_list = []
-		lines			 = []
+		lines			 = None
 		si				= None
 		thinfo			= thread_info_t(0, 0)
 
 		try:
 			f = open(self.__file)
+			# 检测平台信息
 			anlyse = stack_anlyse_intf_t()
 			self.__thread_info_intf = anlyse.probe(f)
 			if None == self.__thread_info_intf:
 				print('cannot decide the os type')
 				exit()
 
+			# 每个栈汇总放在链表中
 			f.seek(0, 0)
 			for line in f:
 				line = line.rstrip('\n').rstrip('\r').strip()
@@ -447,7 +575,8 @@ class StackParser:
 					continue
 
 				if False == self.__thread_info_intf.is_new_thread(line):
-					lines.append(line)
+					if lines != None:
+						lines.append(line)
 				else:
 					if None != si:
 						si.set_lines(lines)
@@ -476,11 +605,13 @@ class StackParser:
 	def parse(self):
 		self.__preparse()
 
+	# 汇总所有线程信息
 	def get_stack_summary(self):
 		if len(self.__stack_summary) > 0:
 			return self.__stack_summary
 
 		for si in self.__stack_list:
+			# 获取线程栈信息签名并统计汇总
 			signature  = self.__thread_info_intf.signature(si)
 			stack_info = self.__stack_summary.get(signature)
 			if None == stack_info:
@@ -502,12 +633,17 @@ class StackParser:
 
 		try:
 			output = OutputHelper()
+			thread_num_report = "thread count: " + str(len(self.__stack_list))
+			if file != None:
+				file.write(thread_num_report + "\n")
+			else:
+				output.println(thread_num_report)
+
+			# 按照出现的次数和栈长度来排序
 			sorted_stack = sorted(self.get_stack_summary().values(), key=lambda x: (x[1], len(x[0])), reverse=True)
 			for stack in sorted_stack:
 				times  = stack[1]
-				outputstr = str(times) + ' time'
-				if times > 1:
-					outputstr += 's'
+				outputstr = "count = " + str(times)
 
 				if file != None:
 					file.write(outputstr + "\n")
